cc170eb72affd66775eb01145fa0e09e2fe550ac
shaolei528 <zuoranzhang@gmail.com>
2026-09-10 15:44:04 +0630

    Add V16 public relay endpoint config

--- survival-config.js
+++ survival-config.js
@@ -1,4 +1,5 @@
 window.ABYSSAL_CONFIG={
+  RELAY_URL:'',
   SUPABASE_URL:'https://kqwlkleuguixkgutwuda.supabase.co',
   SUPABASE_KEY:'sb_publishable_F5j_5pFw4tTDvYLe-mxzXQ_S1l37mUJ',
   WORLD_CHANNEL:'abyssal-wake-public-v1'
